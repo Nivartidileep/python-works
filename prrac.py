d7e809bebb9826c5492a7c175e016e1fdc02c3ce
Dileep Nivarti <nivarthiusha936@gmail.com>
2026-09-17 06:31:00 -0700

    17 sept 2026 practice

---
+++ prrac.py
@@ -0,0 +1,72 @@
+'''for i in range(3):
+    marks = int(input('Enter marks'))
+    if marks>100 or marks <0:
+        print('Invalid marks entred')
+    elif marks>=90:
+        print('Grade: A')
+        print('Remark : outstanding')
+    elif marks>=80:
+        print('Grade: B')
+        print('Remark : Excellent')
+    elif marks>=70:
+        print('Grade : C')
+        print('Remark : Good')
+    elif marks>=60:
+        print('Grade : D')
+        print('Remark :Fair, needs improvement')
+    elif marks>=50:
+        print('Grade : E')
+        print('Remark :Poor, needs serious improvement')
+    elif marks<=50 and marks >=0:
+        print('Grade : F')
+        print('Remark : Failed, needs to reappear')
+
+for i in range(2):
+    month = int(input("Enter month number:"))
+    if month == 12 or month== 1 or month==2:
+        print('Season:Winter')
+    elif month == 3 or month==  4 or month== 5:
+        print('Season:Spring')
+    elif month == 6 or month== 7 or month== 8:
+        print('Season:Summer')
+    elif month == 9 or month== 10 or month== 11:
+        print('Season:Autumn')
+    elif month >= 13 or month <=0:
+        print("Invalid month entered")
+        
+
+'''
+for i in range(5):
+    number = int(input("Enter a number:"))
+    if  number == 0:
+        print('Zero is neither even nor odd')
+    elif  number <0 and number%2==0:
+        print('Negative Even Number')
+    elif  number <0 and number%1==0:
+        print('Negative Odd Number')
+    elif  number%2==0:
+        print('Even Number')
+    elif  number%1==0:
+        print('odd Number')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
